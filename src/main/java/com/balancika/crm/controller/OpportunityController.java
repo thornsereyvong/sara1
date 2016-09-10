15e73a6269da72fc52f9ec808884aa9c8214e16c
Vichet Chann <vchann@balancika.local>
2016-09-10 13:29:50 +0700

    sadfs

--- src/main/java/com/balancika/crm/controller/OpportunityController.java
+++ src/main/java/com/balancika/crm/controller/OpportunityController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.spel.ast.OpInc;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +15,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.balancika.crm.model.CrmMeeting;
 import com.balancika.crm.model.CrmOpportunity;
+import com.balancika.crm.services.CrmCallService;
+import com.balancika.crm.services.CrmEventService;
+import com.balancika.crm.services.CrmMeetingService;
+import com.balancika.crm.services.CrmNoteService;
 import com.balancika.crm.services.CrmOpportunityService;
+import com.balancika.crm.services.CrmTaskService;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -25,6 +32,21 @@ public class OpportunityController {
 	
 	@Autowired
 	private CrmOpportunityService opService;
+	
+	@Autowired
+	private CrmNoteService noteService;
+	
+	@Autowired
+	private CrmTaskService taskService;
+	
+	@Autowired
+	private CrmEventService eventService;
+	
+	@Autowired
+	private CrmCallService callService;
+	
+	@Autowired
+	private CrmMeetingService meetingService;
 
 	@RequestMapping(value="/list_all", method = RequestMethod.GET, produces = "application/json")
 	public ResponseEntity<Map<String, Object>> listOpportunties(){
@@ -67,6 +89,29 @@ public class OpportunityController {
 		map.put("MESSAGE", "FAILED");
 		map.put("STATUS", HttpStatus.NOT_FOUND.value());
 		return new ResponseEntity<Map<String,Object>>(map, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/view", method = RequestMethod.POST, produces = "application/json")
+	public ResponseEntity<Map<String, Object>> listInformationRelatedToOpportunity(@RequestBody String json){
+		
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, String> jsonMap = new HashMap<String, String>();
+		try {
+			jsonMap = mapper.readValue(json, new TypeReference<Map<String, String>>() {});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("MESSAGE", "SUCCESS");
+		map.put("STATUS", HttpStatus.OK.value());
+		map.put("OPPORTUNITY", opService.findOpportunityById(jsonMap.get("opId").toString()));
+		map.put("CALLS", callService.listCallsRelatedToOpportunity(jsonMap.get("opId").toString()));
+		map.put("TASKS", taskService.listTasksRelatedToOpportunity(jsonMap.get("opId").toString()));
+		map.put("MEETINGS",meetingService.listTasksRelatedToOpportunity(jsonMap.get("opId").toString()));
+		map.put("NOTES", noteService.listNotesRelatedToOpportunity(jsonMap.get("opId").toString()));
+		map.put("EVENTS", eventService.listEventsRelatedToOpportunity(jsonMap.get("opId").toString()));
+		return new ResponseEntity<Map<String,Object>>(map, HttpStatus.OK);
+		
 	}
 	
 	@RequestMapping(value="/list/{opId}", method = RequestMethod.GET, produces = "application/json")
