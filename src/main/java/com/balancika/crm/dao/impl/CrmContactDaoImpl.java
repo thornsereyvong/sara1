67ded594e5fd8c2351aa900a63fe5d75495d3c5f
Vichet Chann <vchann@balancika.local>
2016-09-17 09:23:31 +0700

    sads

--- src/main/java/com/balancika/crm/dao/impl/CrmContactDaoImpl.java
+++ src/main/java/com/balancika/crm/dao/impl/CrmContactDaoImpl.java
@@ -125,6 +125,7 @@ public class CrmContactDaoImpl extends CrmIdGenerator implements CrmContactDao {
 		Criteria criteria = session.createCriteria(CrmContact.class);
 		criteria.setProjection(Projections.projectionList()
 							.add(Projections.property("conID"), "conID")
+							.add(Projections.property("conSalutation"), "conSalutation")
 							.add(Projections.property("conFirstname"), "conFirstname")
 							.add(Projections.property("conLastname"), "conLastname"));
 		criteria.add(Restrictions.eqOrIsNull("conReportTo", null));
