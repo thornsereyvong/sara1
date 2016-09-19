9bc32fb88d072e80d5b321177b78ddf00fdbd811
Vichet Chann <vchann@balancika.local>
2016-09-19 17:56:23 +0700

    werfwe

--- src/main/java/com/balancika/crm/utilities/DateTimeOperation.java
+++ src/main/java/com/balancika/crm/utilities/DateTimeOperation.java
@@ -7,12 +7,18 @@ public class DateTimeOperation {
 
 	public LocalDateTime convertStringToLocalDateTime(String date) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy h:mm a");
+		if(date == null || date.equals("")){
+			return null;
+		}
 		LocalDateTime localDateTime = LocalDateTime.parse(date, formatter);
 		return localDateTime;
 	}
 	
 	public String reverseLocalDateTimeToString(LocalDateTime date){
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy h:mm a");
+		if(date == null){
+			return null;
+		}
 		String reverseDate = date.format(formatter);
 		return reverseDate;
 	}
