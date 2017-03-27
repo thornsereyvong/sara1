8bbcfb653311b3d06a858681cb2328deeeefc3a7
channvichet <vichet297@gmail.com>
2017-03-27 18:09:10 +0700

    asdasd

--- src/main/java/com/balancika/crm/utilities/AppUtilities.java
+++ src/main/java/com/balancika/crm/utilities/AppUtilities.java
@@ -9,23 +9,43 @@ import java.util.Map;
 
 public class AppUtilities {
 
-	public Map<String, Object> aliasToMap(ResultSet rs, String mapKey){
-		ResultSetMetaData md;
-		Map<String, Object> map = new HashMap<String, Object>();
+	public static Object aliasToMaps(ResultSet rs){
 		try {
-			md = rs.getMetaData();
-			int columns = md.getColumnCount();
-				ArrayList<Object> list = new ArrayList<Object>();
-				while (rs.next()){
-					HashMap<String, Object> row = new HashMap<String, Object>(columns);
-					for(int i=1; i<=columns; ++i){           
-						row.put(md.getColumnLabel(i),rs.getObject(i));
-					}
-			    list.add(row);
-			  }
+			ArrayList<Map<String, Object>> arr = new ArrayList<>();
+			ResultSetMetaData meta = (ResultSetMetaData) rs.getMetaData();
+			Map<String, Object> map = null; 
+			while(rs.next()){
+				 map = new HashMap<>();
+				 for (int i = 1; i <= meta.getColumnCount(); i++) {
+	                String key = meta.getColumnLabel(i);
+	                Object value = rs.getObject(i);
+	                map.put(key, value);
+	            }
+				arr.add(map);
+			}
+			return arr;			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return map;
+		return null;
+	}
+	
+	public static Object aliasToSingleMap(ResultSet rs){
+		try {
+			ResultSetMetaData meta = (ResultSetMetaData) rs.getMetaData();
+			Map<String, Object> map = null; 
+			while(rs.next()){
+				 map = new HashMap<>();
+				 for (int i = 1; i <= meta.getColumnCount(); i++) {
+	                String key = meta.getColumnLabel(i);
+	                Object value = rs.getObject(i);
+	                map.put(key, value);
+	            }
+			}
+			return map;			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
