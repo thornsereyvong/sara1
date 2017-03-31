bbacaa7c4383a23531b2460073f8a120e944e38d
channvichet <vichet297@gmail.com>
2017-03-31 10:32:05 +0700

    sdfasdf

--- src/main/java/com/balancika/crm/controller/FileUploadController.java
+++ src/main/java/com/balancika/crm/controller/FileUploadController.java
@@ -1,6 +1,10 @@
 package com.balancika.crm.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -9,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,6 +30,30 @@ public class FileUploadController {
 
 	@Autowired
 	private MultipartResolver multipartResolver;
+	
+	@RequestMapping(value="/{srcFolder}",method = RequestMethod.POST)
+	public ResponseEntity<Map<String, Object>> upload(@RequestParam(value = "file", required = false) MultipartFile[] files,HttpServletRequest request, @PathVariable("srcFolder") String srcFolder){
+		Map<String, Object> map = new HashMap<String, Object>();
+		List<String> fileNames= new ArrayList<String>();
+        String savePath = request.getSession().getServletContext().getRealPath("/");
+        String filePath = savePath + "resources/" + srcFolder;
+        String pathAndFilename = "/resources/"+srcFolder;
+        File f = new File(filePath);
+        String randomFileName = "";
+        if(!f.exists()){
+        	f.mkdir();
+        }
+        for(MultipartFile file : files)
+        	try {
+        		randomFileName = UUID.randomUUID()+"."+file.getOriginalFilename().split("\\.")[1];
+				FileCopyUtils.copy(file.getBytes(), new File(filePath +File.separator+ randomFileName));
+				fileNames.add(pathAndFilename+"/"+randomFileName);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+        map.put("fileNames", fileNames);
+		return new ResponseEntity<Map<String,Object>>(map, HttpStatus.OK);
+	}
 	
 	@RequestMapping(method = RequestMethod.POST, value = "/image/{srcFolder}")
 	public ResponseEntity<Map<String, Object>> uploadimage(
